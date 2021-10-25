93eaa6eb128962ea5a845cd02371361c7dafa032
assem ben sbih <assembensbih30@gmail.com>
2021-10-25 15:17:45 +0200

    challenge day 26

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+    def ask
+      @ask = params[:ask]
+    end
+  
+    def answer
+      @ask = params[:ask]
+      if @ask.include? 'work'
+        @answer = 'Great!'
+      elsif @ask.include? '?'
+        @answer = 'Silly question, get dressed and go to work!'
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
 end
